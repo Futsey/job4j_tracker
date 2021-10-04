603532d085ad4877fbd59dee9fd386382f3f0fea
Futsey <shmopls@gmail.com>
2021-10-04 09:42:46 +0500

    1.6. Взаимодействие объектов [#166087 #211673]

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -5,9 +5,9 @@ public class Ball {
     public Ball tryRun(boolean condition) {
         boolean result = false;
         if (!condition) {
-            System.out.println("RIP: Kolobock. Bright memory of this yeast ball");
+            System.out.println("RIP: Kolobok. Bright memory of this yeast ball");
         } else {
-            System.out.println("Kolobock run away!!!");
+            System.out.println("Kolobok run away!!!");
         }
         return null;
     }
--- src/main/java/ru/job4j/oop/BallStory.java
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -3,12 +3,12 @@ package ru.job4j.oop;
 public class BallStory {
 
     public static void main(String[] args) {
-        Ball kolobock = new Ball();
+        Ball kolobok = new Ball();
         Hare greyEar = new Hare();
         Wolf sharpTooth = new Wolf();
         Fox fireTail = new Fox();
-        greyEar.tryEat(kolobock.tryRun(false));
-        sharpTooth.tryEat(kolobock.tryRun(false));
-        fireTail.tryEat(kolobock.tryRun(true));
+        greyEar.tryEat(kolobok.tryRun(false));
+        sharpTooth.tryEat(kolobok.tryRun(false));
+        fireTail.tryEat(kolobok.tryRun(true));
     }
 }
