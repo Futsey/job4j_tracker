06bc480d6152e74001f39f096e0a03a965b09593
Futsey <shmopls@gmail.com>
2021-10-10 08:38:44 +0500

    4. Переопределение [#174890]

--- src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -3,8 +3,8 @@ package ru.job4j.inheritance;
 public class ReportUsage {
 
     public static void main(String[] args) {
-        TextReport report = new TextReport();
-        String text = report.generate("Report's name", "Report's body");
+        JSONReport jsonReport = new JSONReport();
+        String text = jsonReport.generate("name", "body");
         System.out.println(text);
     }
 }
