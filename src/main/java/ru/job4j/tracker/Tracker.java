28764e5df52eb5ab3742e8e0231245174d461a05
Futsey <shmopls@gmail.com>
2021-10-13 11:58:17 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import ru.job4j.tracker.Item;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +24,39 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item name = items[index];
+            if (name != null) {
+                itemsWithoutNull[size] = name;
+                size++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        for (int index = size; index < itemsWithoutNull.length; index++) {
+            System.out.println(itemsWithoutNull[index]);
+        }
+        return itemsWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] sameNameItems = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item name = items[i];
+            if (name != null && name.getName().equals(key)) {
+                sameNameItems[size] = name;
+                size++;
+            }
+        }
+        sameNameItems = Arrays.copyOf(sameNameItems, size);
+        for (int index = size; index < sameNameItems.length; index++) {
+            System.out.println(sameNameItems[index]);
+        }
+        return sameNameItems;
     }
 }
