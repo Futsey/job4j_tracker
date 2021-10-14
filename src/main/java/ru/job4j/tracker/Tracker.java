281494b7ea3db805c109ea9f51daf057ffa69146
Futsey <shmopls@gmail.com>
2021-10-14 13:36:50 +0500

    7. Метод удаления заявки Tracker.delete [#211749 #215870]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -64,7 +64,7 @@ public class Tracker {
     * возвращаем true, если процесс состоялся
     *
     * возвращаем false, если процесс не состоялся
-    */
+     */
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index != -1) {
@@ -77,8 +77,8 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        System.arraycopy(items, index + 1, items, index, size - index - 1);
         if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
             return true;
---
+++ src/main/java/ru/job4j/validate/DivByZero.java
@@ -0,0 +1,4 @@
+package ru.job4j.validate;
+
+public class DivByZero {
+}
