e3b89e5cf82bd8eee29dcd4515a676b8e9f625b2
Futsey <shmopls@gmail.com>
2021-10-15 17:21:38 +0500

    2.3. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,9 +10,14 @@ public class StartUI {
             showMenu();
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            if (select != 6) {
-                System.out.println("Пользователь выбрал: " + select);
-            } else {
+            if (select == 0) {
+                System.out.println("=== Create a new Item ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 6) {
                 run = false;
             }
         }
@@ -30,7 +35,7 @@ public class StartUI {
         }
     }
 
-    public static void main (String[]args) {
+    public static void main(String[]args) {
         Scanner scanner = new Scanner(System.in);
         Tracker tracker = new Tracker();
         new StartUI().init(scanner, tracker);
