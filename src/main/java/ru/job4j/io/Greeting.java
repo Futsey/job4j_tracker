10554faddca53fcaca1b38fefbf1314033b6968b
Futsey <shmopls@gmail.com>
2021-10-15 10:54:13 +0500

    1. Чтение из консоли. Класс Scanner. [#179514]

---
+++ src/main/java/ru/job4j/io/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome! What`s your name?");
+        String name = input.nextLine();
+        System.out.println(name + " we glad to see you!");
+    }
+}
---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class MagicBall {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Я великий Оракул. Что ты хочешь узнать? ");
+        String answer = input.nextLine();
+        int randomize = new Random().nextInt(3);
+            if (randomize == 0) {
+                System.out.println("Да");
+            }
+            if (randomize == 1) {
+                System.out.println("Нет");
+            } else {
+                System.out.println("Может быть...");
+            }
+    }
+}
+
