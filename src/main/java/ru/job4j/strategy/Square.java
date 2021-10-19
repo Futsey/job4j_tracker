15feada92a722e1b67be9cd19496b6c8e4f14e81
Futsey <shmopls@gmail.com>
2021-10-19 14:15:35 +0500

    7. Шаблон проектирования - Стратегия [#786 #217914]

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -5,7 +5,7 @@ public class Square implements Shape {
     @Override
     public String draw() {
         String ln = System.lineSeparator();
-        return "________" + ln
+        return    "________" + ln
                 + "|      |" + ln
                 + "|______|" + ln;
     }
