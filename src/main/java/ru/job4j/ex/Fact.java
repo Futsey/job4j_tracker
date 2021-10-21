2ab171ccee21b9f27fd0f51770e4721d1c26fb02
Futsey <shmopls@gmail.com>
2021-10-21 19:35:33 +0500

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,21 @@
+package ru.job4j.ex;
+
+public class Fact {
+
+    public static void main(String[] args) {
+        new Fact().calc(-1);
+    }
+
+    public int calc(int n) {
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("The value cannot be negative. "
+                    + "It should be positive number");
+        }
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+
+        }
+        return rsl;
+    }
+}
