8a78e65d266e64397ac3da021cc1006507bbcb8b
Futsey <shmopls@gmail.com>
2021-10-27 16:03:40 +0500

    0.5. Иерархия исключений и множественный catch. [#219368 #221106]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -3,18 +3,17 @@ package ru.job4j.ex;
 public class UserStore {
 
     public static User findUser(User[] users, String login) throws UserNotFoundException {
-        for (int index = 0; index < users.length; index++) {
-            if (login.equals(users[index].getUserName())) {
-                User user = users[index];
+        for (User user : users) {
+            if (login.equals(user.getUserName())) {
                 System.out.println("Welcome " + user.getUserName());
-                return users[index];
+                return user;
             }
         }
         throw new UserNotFoundException("Can`t find user in the list");
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if ((!user.isValid()) || (user.getUserName().length() < 3)) {
+        if (!user.isValid() || user.getUserName().length() < 3) {
             throw new UserInvalidException("User is not valid or length of your name"
                     + " less then three chars");
         }
@@ -25,7 +24,7 @@ public class UserStore {
         User[] users = {
                 new User("Petr Arsentev", true),
                 new User("Igor Arsentev", false),
-                new User("Andrew Petrushin", true),
+                new User("Andrew Petrushin", false),
                 new User("PA", true),
         };
         try {
