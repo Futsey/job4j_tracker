da01abbd855e123e7b8ab1988de71448586c12bc
Futsey <shmopls@gmail.com>
2021-10-27 08:43:51 +0500

    0. Что такое исключение. [#3821 #218878]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,24 +2,26 @@ package ru.job4j.ex;
 
 public class FindEl {
 
-    public static int indexOf(String[] value, String key) {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == null) {
-                try {
-                    throw new ElementNotFoundException("Can`t find element in value");
-                } catch (ElementNotFoundException e) {
-                    e.printStackTrace();
-                    System.out.println("Index of array, where exception happened: " + i);
-                }
+            if (key.equals(value[i])) {
+                rsl = i;
+                break;
             }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Can`t find element in value");
         }
         return rsl;
     }
 
     public static void main(String[] args) {
-        FindEl testValue = new FindEl();
         String[] array = {"1", "2", "3", null, "4", "5"};
-        testValue.indexOf(array, "4");
+        try {
+            System.out.println(indexOf(array, "keyword"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
