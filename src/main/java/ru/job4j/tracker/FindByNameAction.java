9391fca3976e0443f0e0ecc0f23c57ef0e2b0e33
Futsey <shmopls@gmail.com>
2021-12-07 09:47:10 +0500

    6. Изменить программу Tracker из 2-го модуля [#10039 #236673]

--- src/main/java/ru/job4j/tracker/FindByNameAction.java
+++ src/main/java/ru/job4j/tracker/FindByNameAction.java
@@ -19,7 +19,7 @@ public class FindByNameAction implements UserAction {
     public boolean execute(Input input, Tracker tracker) {
         out.println("=== Find items by name ===");
         String name = input.askStr("Enter name: ");
-        List<Item> items = (List<Item>) tracker.findByName(name);
+        List<Item> items = tracker.findByName(name);
         if (items.size() > 0) {
             for (Item item : items) {
                 out.println(item);
