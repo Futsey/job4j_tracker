38a5b2354f01f4490f4550b9459ce1afb9ab7b9d
Futsey <shmopls@gmail.com>
2021-12-15 17:39:52 +0500

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+
+    public static void main(String[] args) {
+        HashMap<String, String> user = new HashMap<>();
+        user.put("boba@bob.com", "Bob Thornton");
+        for (Map.Entry<String, String> el : user.entrySet()) {
+            System.out.println(el);
+        }
+        user.put("Andrew@andrew.com", "Andrew Petrushin");
+        user.put("Andrew@andrew.com", "Andrey petrushin");
+        for (String key : user.keySet()) {
+            String value = user.get(key);
+            System.out.println(key + "|" + value);
+        }
+    }
+}
