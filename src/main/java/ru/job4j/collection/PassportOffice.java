7aae5d30848adc6ec17e642ef4114131b5f1c005
Futsey <shmopls@gmail.com>
2021-12-17 09:07:06 +0500

    1. Паспорт и жители [#212656 #240737]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -5,23 +5,11 @@ import java.util.Map;
 
 public class PassportOffice {
 
-    /*
-    * добавили поле с перечнем граждан
-     */
     private Map<String, Citizen> citizens = new HashMap<>();
 
     public boolean add(Citizen citizen) {
-        /*
-        * по умолчанию вносимый персонаж у нас присутствует в списке
-         */
         boolean rsl = false;
-        /*
-        * если перечень не содержит данных о паспорте
-         */
         if (!citizens.containsKey(citizen.getPassport())) {
-            /*
-            * то добавляем нового гражданина и говорим сервису, что персонаж уникален (true)
-             */
             citizens.put(citizen.getPassport(), citizen);
             rsl = true;
         }
@@ -29,10 +17,6 @@ public class PassportOffice {
     }
 
     public Citizen get(String passport) {
-        /*
-        * заглядываем в паспорт и возвращаем значение в тест
-         */
-        citizens.get(passport);
         return citizens.get(passport);
     }
 }
--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -13,4 +13,31 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void addDublicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen1 = new Citizen("2f44a", "Arsen Petrov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen1);
+        assertFalse(false);
+    }
+
+    @Test
+    public void addThreeCitWithDublicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen1 = new Citizen("gzqa3", "Petya Arsentev");
+        Citizen citizen2 = new Citizen("6fa", "Petr Arsentev");
+        Citizen citizen3 = new Citizen("2f44a", "Arsen Petrov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen1);
+        office.add(citizen2);
+        office.add(citizen3);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+        assertThat(office.get(citizen1.getPassport()), is(citizen1));
+        assertThat(office.get(citizen2.getPassport()), is(citizen2));
+        assertFalse(false);
+    }
 }
