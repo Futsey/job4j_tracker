8380d8379a32b97f64d5e36ec603b277f51bfc81
Futsey <shmopls@gmail.com>
2021-12-28 16:21:28 +0500

    0. Сортировка [#10096 #244814]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -52,12 +52,12 @@ public class Item {
             return false;
         }
         Item item = (Item) k;
-        return id == item.id && name.equals(item.name) && created.equals(item.created);
+        return id == item.id && name.equals(item.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, created);
+        return Objects.hash(id, name);
     }
 
     @Override
