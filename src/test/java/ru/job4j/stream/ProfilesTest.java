497d6ef09ac2f4357e44cb361070b455b53a344c
Andrew Petrushin <shmopls@gmail.com>
2022-02-25 16:17:59 +0500

    Уникальность элементов и сортировка. [#110225]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -30,9 +30,9 @@ public class ProfilesTest {
         Address second = new Address("Tarko-City", "Lenina", 12, 23);
         Address third = new Address("New Urengoy", "Popova", 33, 8);
         Address fourth = new Address("New Urengoy", "Lenina", 19, 66);
-        Address fifth = new Address("New Urengoy", "Octyabrskaya", 12, 21);
-        Address sixth = new Address("New Urengoy", "Truda", 17, 26);
-        Address seventh = new Address("New Urengoy", "50 let USSR", 2, 12);
+        Address fifth = new Address("Noyabrsk", "Octyabrskaya", 12, 21);
+        Address sixth = new Address("Uzhgorod", "Truda", 17, 26);
+        Address seventh = new Address("Omsk", "50 let USSR", 2, 12);
         List<Profile> profiles = Arrays.asList(
                 new Profile(first),
                 new Profile(second),
@@ -44,6 +44,27 @@ public class ProfilesTest {
         );
         List<Address> result = Profiles.collect(profiles);
         List<Address> expected = Arrays.asList(first, second, third, fourth, fifth, sixth, seventh);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenTestMethodCollectSortWithoutDuplicate() {
+        Address first = new Address("City1", "Street1", 1, 1);
+        Address second = new Address("City2", "Street2", 2, 2);
+        Address third = new Address("City3", "Street3", 3, 3);
+        Address fourth = new Address("City1", "Street1", 1, 1);
+        Address fifth = new Address("City2", "Street2", 2, 2);
+        Address seventh = new Address("City3", "Street3", 3, 3);
+        List<Profile> profiles = Arrays.asList(
+                new Profile(second),
+                new Profile(fourth),
+                new Profile(fifth),
+                new Profile(first),
+                new Profile(seventh),
+                new Profile(third)
+        );
+        List<Address> result = Profiles.collectSortWithoutDuplicate(profiles);
+        List<Address> expected = Arrays.asList(first, second, third);
         assertEquals(expected, result);
     }
 }
