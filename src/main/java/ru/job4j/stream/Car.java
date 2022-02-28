70c86dd47f10584ecbc8de1350d6e0262a93c5a2
Andrew Petrushin <shmopls@gmail.com>
2022-02-28 16:43:58 +0500

    Шаблоны проектирования в Stream API [#310012]

---
+++ src/main/java/ru/job4j/stream/Car.java
@@ -0,0 +1,92 @@
+package ru.job4j.stream;
+
+import java.time.LocalDate;
+
+public class Car {
+
+    private String brand;
+    private String model;
+    private LocalDate created;
+    private double volume;
+    private String color;
+
+    static class Builder {
+        private String brand;
+        private String model;
+        private LocalDate created;
+        private double volume;
+        private String color;
+
+        Builder buildBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        Builder buildModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        Builder buildCreated(LocalDate created) {
+            this.created = created;
+            return this;
+        }
+
+        Builder buildVolume(double volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        Builder buildColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        Car build() {
+            Car car = new Car();
+            car.brand = brand;
+            car.model = model;
+            car.created = created;
+            car.volume = volume;
+            car.color = color;
+            return car;
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Builder()
+                .buildBrand("Toyota")
+                .buildModel("Camry")
+                .buildCreated(LocalDate.of(2021, 6, 1))
+                .buildVolume(2.5)
+                .buildColor("Red")
+                .build();
+        System.out.println(car);
+
+        Car car2 = new Builder()
+                .buildBrand("Toyota")
+                .buildModel("Prius")
+                .buildColor("White")
+                .buildCreated(LocalDate.of(2001, 4, 12))
+                .build();
+        System.out.println(car2);
+
+        Car car3 = new Builder()
+                .buildBrand("BMW")
+                .buildColor("black")
+                .buildCreated(LocalDate.of(1989, 1, 3))
+                .build();
+        System.out.println(car3);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "brand='" + brand + '\''
+                + ", model='" + model + '\''
+                + ", created=" + created
+                + ", volume=" + volume
+                + ", color='" + color + '\''
+                + '}';
+    }
+}
