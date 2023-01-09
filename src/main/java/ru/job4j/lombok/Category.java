19266407ea4632ba389826b806e391794c716d6d
Fut <shmopls@gmail.com>
2023-01-09 06:37:08 +0500

    @Setter, @Getter и другие

---
+++ src/main/java/ru/job4j/lombok/Category.java
@@ -0,0 +1,18 @@
+package ru.job4j.lombok;
+
+import lombok.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
+public class Category {
+
+    @NonNull
+    @Getter
+    @EqualsAndHashCode.Include
+    private int id;
+    
+    @Getter
+    @Setter
+    @EqualsAndHashCode.Exclude
+    private String name;
+}
